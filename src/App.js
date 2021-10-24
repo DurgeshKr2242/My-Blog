a7d615ae04f3ead89df4f7371216f57f14901dff
DurgeshKr2242 <code.durgesh@gmail.com>
2021-10-24 22:36:38 +0530

    added react router 🖇🔗

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
 import Feed from "./Components/Feed/Feed";
+import NewPost from "./Components/NewPost/NewPost";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Hero />
-      <Feed />
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Hero />
+            <Feed />
+          </Route>
+          <Route path="/new">
+            <NewPost />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
---
+++ src/Components/NewPost/NewPost.js
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./NewPost.module.css";
+const NewPost = () => {
+  return <div className={styles.newPostMainContainer}></div>;
+};
+
+export default NewPost;
